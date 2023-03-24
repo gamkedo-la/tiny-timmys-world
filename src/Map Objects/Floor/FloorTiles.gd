e92100338e5962672e328ce3d69e539f149d9a14
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2023-03-24 21:14:22 +0000

    Tileset floor ready

--- src/Map Objects/Floor/FloorTiles.gd
+++ src/Map Objects/Floor/FloorTiles.gd
@@ -2,29 +2,54 @@ extends Node2D
 
 @export var tile_set: TileSet
 
-var viewport_width: int = 0
-var viewport_height: int = 0
-var pixel_cell_counter: float
-var pixel_total_movement_counter: float
-var floor_tilemap: TileMap
-const SPEED: float = -16.0
+var min_coords_x: int
+var max_coords_x: int
+
+var min_coords_y: int
+var max_coords_y: int
+
+const SPEED: float = -20.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	min_coords_x = 0
+	max_coords_x = get_viewport_rect().size.x / tile_set.tile_size.x
+	
+	print_debug(max_coords_x)
+
+	min_coords_y = 0
+	max_coords_y = get_viewport_rect().size.y / tile_set.tile_size.y
+	print_debug(max_coords_y)
+	
+	_spawn_initial_random_floor()
 	_spawn_new_random_floor()
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	for child in get_children():
+		child.position.x += SPEED * delta
+		if child.position.x < -get_viewport_rect().size.x:
+			child.queue_free()
+			_spawn_new_random_floor()
 	
 
+func _spawn_initial_random_floor() -> void:
+	var new_floor: TileMap = TileMap.new()
+	new_floor.tile_set = tile_set
+	
+	for n in max_coords_x:
+		new_floor.set_cell(0, Vector2i(n, max_coords_y), 0, Vector2(0, 0), 0)
+		
+	add_child(new_floor)
+	
 func _spawn_new_random_floor() -> void:
 	var new_floor: TileMap = TileMap.new()
 	new_floor.tile_set = tile_set
 	
-	for n in 25:
-		new_floor.set_cell(0, Vector2i(n, 5), 0, Vector2(0, 0), 0)
-		
-	add_child(new_floor)
+	for n in max_coords_x:
+		new_floor.set_cell(0, Vector2i(n, max_coords_y), 0, Vector2(0, 0), 0)
+	
+	new_floor.position.x += get_viewport_rect().size.x
+	add_child(new_floor)	
 
