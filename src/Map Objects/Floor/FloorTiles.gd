a53f5c2f21320ecdc9bf16e5a284edf0910e9b0e
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2023-04-03 21:31:18 +0100

    Created Backyard Garden level + Added initial floor tilemap

--- src/Map Objects/Floor/FloorTiles.gd
+++ src/Map Objects/Floor/FloorTiles.gd
@@ -11,14 +11,16 @@ var max_coords_y: int
 const SPEED: float = -20.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print_debug("Tile Set Size: ", tile_set.tile_size.x)
+	
 	min_coords_x = 0
 	max_coords_x = get_viewport_rect().size.x / tile_set.tile_size.x
 	
-	print_debug(max_coords_x)
+	print_debug("max_coords_x: ",max_coords_x)
 
 	min_coords_y = 0
 	max_coords_y = get_viewport_rect().size.y / tile_set.tile_size.y
-	print_debug(max_coords_y)
+	print_debug("max_coords_y: ",max_coords_x)
 	
 	_spawn_initial_random_floor()
 	_spawn_new_random_floor()
@@ -39,7 +41,8 @@ func _spawn_initial_random_floor() -> void:
 	new_floor.tile_set = tile_set
 	
 	for n in max_coords_x:
-		new_floor.set_cell(0, Vector2i(n, max_coords_y), 0, Vector2(0, 0), 0)
+		print_debug("Adding cell to coords: ", Vector2i(n, max_coords_y))
+		new_floor.set_cell(0, Vector2i(n, max_coords_y), 0, Vector2(2, 0), 0)
 		
 	add_child(new_floor)
 	
@@ -48,7 +51,7 @@ func _spawn_new_random_floor() -> void:
 	new_floor.tile_set = tile_set
 	
 	for n in max_coords_x:
-		new_floor.set_cell(0, Vector2i(n, max_coords_y), 0, Vector2(0, 0), 0)
+		new_floor.set_cell(0, Vector2i(n, max_coords_y), 0, Vector2(2, 0), 0)
 	
 	new_floor.position.x += get_viewport_rect().size.x
 	add_child(new_floor)	
