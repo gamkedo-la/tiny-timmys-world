32988d02bff196e089d6fb11a5238aec20451506
ThGnommy <th.brandoli@gmail.com>
2023-04-07 21:45:43 +0200

    added parallax bg to main menu

--- src/UI/MainMenu.gd
+++ src/UI/MainMenu.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var quit_button = $Panel/VBoxContainer/Quit
 @onready var fullscreen_toggle = $Panel/VBoxContainer/FullscreenToggle
 @onready var credits_button = $Panel/VBoxContainer/Credits
+@onready var parallax = $ParallaxBackground
+
+var parallax_bg_velocity = 1
 
 func _ready() -> void:
 	# Hide the Quit button if the game runs in a web browser
@@ -11,13 +14,14 @@ func _ready() -> void:
 		quit_button.hide()
 		fullscreen_toggle.hide()
 
+func _process(delta: float) -> void:
+	parallax.scroll_base_offset.x -= parallax_bg_velocity
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(level_scene)
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_fullscreen_toggle_toggled(button_pressed: bool) -> void:
 	Global.set_fullscreen(button_pressed)
