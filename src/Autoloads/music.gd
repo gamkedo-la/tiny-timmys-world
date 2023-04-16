a25450f070fc17a675fde6ba7a0d5963304e5de1
Oli Brock <kbshields@gmail.com>
2023-04-16 10:22:34 -0400

    Garden Theme, Music Singleton and AudioBus setup

---
+++ src/Autoloads/music.gd
@@ -0,0 +1,26 @@
+extends Node
+
+@onready var level_bgm: AudioStreamPlayer = $LevelTheme
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var con_res
+	if not level_bgm.is_connected("finished", restart_bgm):
+		con_res = level_bgm.connect("finished", restart_bgm)
+		assert(con_res == OK)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func load_and_play_levelbgm(track: AudioStream) -> void:
+	level_bgm.stream = track
+	level_bgm.seek(0.0)
+	level_bgm.play()
+
+
+func restart_bgm() -> void:
+	level_bgm.seek(0.0)
+	level_bgm.play()
--- src/Levels/level_template.gd
+++ src/Levels/level_template.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 #var level_score: float
 @export var level_completion_time: float
+@export var level_bgm: AudioStream
 #var level_progress: float
 #var level_elapsed_time: float
 
@@ -12,6 +13,8 @@ func _ready() -> void:
 	PlayerVars.level_progress = 0
 	PlayerVars.level_elapsed_time = 0
 	PlayerVars.level_completion_time = level_completion_time
+	if level_bgm:
+		Music.load_and_play_levelbgm(level_bgm)
 	
 func _process(delta: float) -> void:
 	_update_elapsed_time(delta)
