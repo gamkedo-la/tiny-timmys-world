e3d961e718b825b2ce877beee8c0163bcc3aaf8e
MrPhil <mrphil@mrphilgames.com>
2023-05-01 10:16:35 -0500

    Added simple placeholder, but functional parts for the Aggressive Flying Enemy.  Basically just a copy of the current Flying Enemy.  This includes being a part on the list of enemies that the Enemy Manger spawns, but visually look the same.

---
+++ src/Actors/Enemies/AggressiveFlyingEnemy/AgressiveFlyingEnemy.gd
@@ -0,0 +1,69 @@
+extends AIActor
+
+
+@export var score: int = 30
+@export var health: int = 30
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var debug_label: Label = $DebugLabel
+@onready var sprite: Sprite2D = $Body/Enemy
+@onready var actor_body: Node2D = $Body
+@onready var stomp_detector: Area2D = $Body/StompDetector
+
+var dir = Vector2.LEFT
+
+func _ready() -> void:
+	if !Global.show_debug_labels_enemies:
+		debug_label.visible = false
+	velocity.x = dir.x * SPEED
+
+func _on_stomp_detecter_body_entered(body: Node2D) -> void:
+#	print_debug("Stomped")
+#	print_debug("body.global_position.y: ", body.global_position.y)
+#	print_debug("stomp_detector.global_position.y: ", stomp_detector.global_position.y)
+	if(body.global_position.y > stomp_detector.global_position.y) :
+		return
+	
+	health -= PlayerVars.player_stomp_damage
+	Global.emit_signal("damage_taken", PlayerVars.player_stomp_damage, (get_global_transform() * body.position))
+	
+	if health <= 0:
+		Global.emit_signal("points_scored", score, (get_global_transform() * body.position))
+		Global.tween_eng_halftime()
+		anim_player.play("squish")
+#	queue_free()
+
+
+func _on_bullet_detector_area_entered(area: Area2D) -> void:
+#	print_debug(area.name)
+	health -= PlayerVars.player_slingshot_damage
+	Global.emit_signal("damage_taken", PlayerVars.player_slingshot_damage, (get_global_transform() * self.position))
+	
+	if health <= 0:
+		Global.emit_signal("points_scored", score, (get_global_transform() * area.position))
+		anim_player.play("shot_pop")
+#	queue_free()
+
+func _physics_process(delta: float) -> void:
+	#super(delta)
+	if is_on_wall():
+		dir.x *= -1.0
+#		SPEED *= -1.0
+	velocity.x = SPEED * dir.x
+	
+		
+	if dir.is_equal_approx(Vector2.LEFT) and actor_body.scale.x < 0.0:
+		actor_body.scale.x *= -1.0
+	if dir.is_equal_approx(Vector2.RIGHT) and actor_body.scale.x > 0.0:
+		actor_body.scale.x *= -1.0
+		
+	move_and_slide()
+	if debug_label.visible:
+		_update_debug_label()
+
+
+func _update_debug_label() -> void:
+	debug_label.text = "Vel: " + var_to_str(velocity)
+	debug_label.text += "\ndir: " + var_to_str(dir)
+	debug_label.text += "\nsprite.scale.x " + var_to_str(sprite.scale.x)
+	
+	pass
