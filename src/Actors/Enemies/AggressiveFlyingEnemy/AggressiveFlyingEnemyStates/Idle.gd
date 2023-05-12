b39615a9338a42c466c1a06f0328979362247e2b
MrPhil <mrphil@mrphilgames.com>
2023-05-12 18:10:48 -0500

    WIP Adding some basic states scripts for Aggressive Flying Enemy.

---
+++ src/Actors/Enemies/AggressiveFlyingEnemy/AggressiveFlyingEnemyStates/Idle.gd
@@ -0,0 +1,29 @@
+extends AIState
+
+@onready var RNG = RandomNumberGenerator.new()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func process(delta: float) -> void:
+	state_check()
+	time_in_state += delta
+	pass
+
+
+func physics_process(delta: float) -> void:
+	actor.physics_process(delta)
+
+
+func state_check()->void:
+	if time_in_state > 2.0:
+		_ai_state_machine.transition_to('Sting', {})
+	pass
+
+func enter(msg:Dictionary = {}) -> void:
+	RNG.randomize()
+	actor.ani_player_play("Idle")
+
+	time_in_state = 0.0
+	pass
+
+func exit() -> void:
+	pass
