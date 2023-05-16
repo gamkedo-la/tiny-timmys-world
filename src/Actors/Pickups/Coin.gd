ef1ee5d60f2cbce3241c1bfaa710f45a95a851af
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2023-05-15 23:40:19 -0400

    Get coin to move

--- src/Actors/Pickups/Coin.gd
+++ src/Actors/Pickups/Coin.gd
@@ -1,6 +1,16 @@
-extends Node2D
+extends CharacterBody2D
+class_name Coin
 
+@export var SPEED = 50.0
 @export var score: int = 30
+
+var dir = Vector2.LEFT
+
+func _ready() -> void:
+	velocity.x = dir.x * SPEED
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	Global.emit_signal("points_scored", score, (get_global_transform() * body.position))
