3c4d715af60b14fdf89a347b1db7ebfec84e5382
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2023-06-02 23:03:37 -0400

    Create powerup spawner

---
+++ src/Actors/Pickups/PickupManager.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var rng: RandomNumberGenerator
+var viewport: Rect2
+@export var pickups: Array[PackedScene]
+@export var timer_time: float
+
+func _ready() -> void:
+	rng = RandomNumberGenerator.new()
+	$PickupTimer.wait_time = timer_time
+
+func _on_spawn_timer_timeout() -> void:
+	var random_index = rng.randi_range(0, pickups.size() - 1)
+	var class_to_spawn = pickups[random_index]
+	
+	var object = class_to_spawn.instantiate()
+	object.position = Vector2(position.x, position.y)
+	add_sibling(object)
