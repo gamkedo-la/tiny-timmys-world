57bb063c9872dfb096a9e8128f0416b11aab6c31
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2023-07-06 23:24:53 +0100

    Volume control buttons added to Pause menu

--- src/UI/PauseMenu.gd
+++ src/UI/PauseMenu.gd
@@ -31,3 +31,19 @@ func _on_restart_level_pressed() -> void:
 func _on_back_to_menu_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://src/UI/MainMenu.tscn") 
+
+
+func _on_volume_up_pressed():
+	var music_index= AudioServer.get_bus_index("Master")
+	var current_volume = AudioServer.get_bus_volume_db(music_index)
+	AudioServer.set_bus_volume_db(music_index, current_volume + 5)
+
+
+func _on_volume_down_pressed():
+	var music_index= AudioServer.get_bus_index("Master")
+	var current_volume = AudioServer.get_bus_volume_db(music_index)
+	AudioServer.set_bus_volume_db(music_index, current_volume - 5)
+
+
+func _on_volume_mute_pressed():
+	AudioServer.set_bus_mute(0, not AudioServer.is_bus_mute(0))
