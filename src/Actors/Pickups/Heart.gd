350eb7bb4ab69fedfdea68f4c1ddd14d9dd6843e
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2023-07-06 22:28:56 +0100

    Fix: Health was always maxing out + healing with full health added extra invisible health

--- src/Actors/Pickups/Heart.gd
+++ src/Actors/Pickups/Heart.gd
@@ -8,7 +8,7 @@ class_name Heart
 var dir = Vector2.LEFT
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
-	PlayerVars.player_health = max(PlayerVars.player_health + 2, PlayerVars.player_max_health)
+	PlayerVars.player_health = min(PlayerVars.player_health + 2, PlayerVars.player_max_health)
 	
 	Global.emit_signal("player_health_restored", PlayerVars.player_health, (get_global_transform() * body.position))
 	Global.emit_signal("points_scored", score, (get_global_transform() * body.position))
