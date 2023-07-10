0f995ebde59e40262e125b7dec9e7aad7a48dbc3
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2023-07-10 20:18:03 +0100

    heart sfx

--- src/Actors/Pickups/Heart.gd
+++ src/Actors/Pickups/Heart.gd
@@ -5,11 +5,13 @@ class_name Heart
 @export var SPEED = 50.0
 @export var score: int = 10
 
+var heart_pickup = preload("res://src/Audio/Frog/frog-croak-3.wav")
+
 var dir = Vector2.LEFT
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	PlayerVars.player_health = min(PlayerVars.player_health + 2, PlayerVars.player_max_health)
-	
+	Global.emit_signal("play_sfx", heart_pickup)
 	Global.emit_signal("player_health_restored", PlayerVars.player_health, (get_global_transform() * body.position))
 	Global.emit_signal("points_scored", score, (get_global_transform() * body.position))
 	Global.tween_eng_halftime()
