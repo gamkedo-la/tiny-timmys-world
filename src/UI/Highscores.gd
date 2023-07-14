1cc6d530a214f9b05a4ce0070429b78f25b14ca4
TheHaad <fmuntaz@gmail.com>
2023-07-14 16:20:56 -0400

    [wip] scorekeeper - add high scores to item list

--- src/UI/Highscores.gd
+++ src/UI/Highscores.gd
@@ -1,8 +1,13 @@
 extends Control
-
+var item_list: ItemList
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	item_list = get_node("Panel/Panel/ItemList")
+	item_list.add_item("Rank          Score", null, false)
+	var index = 0
+	for i in PlayerVars.high_scores:
+		item_list.add_item("  %d                 %d" % [index, i] , null, false)
+		index = index + 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
