929f5d72788bcbc55dd6edcf0ba17da776f4738a
TheHaad <fmuntaz@gmail.com>
2023-07-14 16:31:10 -0400

    [wip] scorekeeper - add no scores text if no high scores available

--- src/UI/Highscores.gd
+++ src/UI/Highscores.gd
@@ -3,11 +3,14 @@ var item_list: ItemList
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	item_list = get_node("Panel/Panel/ItemList")
-	item_list.add_item("Rank          Score", null, false)
-	var index = 0
-	for i in PlayerVars.high_scores:
-		item_list.add_item("  %d                 %d" % [index, i] , null, false)
-		index = index + 1
+	if PlayerVars.high_scores.size() == 0:
+		item_list.add_item(" No Scores Yet...")
+	else:
+		item_list.add_item("Rank          Score", null, false)
+		var index = 0
+		for i in PlayerVars.high_scores:
+			item_list.add_item("  %d                 %d" % [index, i] , null, false)
+			index = index + 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
