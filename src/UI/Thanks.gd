8d94c03b83e77916952095526af7f83ee6c8213e
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2023-07-14 22:14:11 +0100

    Tiny Timmy main music sounds on Thanks for Playing menu

--- src/UI/Thanks.gd
+++ src/UI/Thanks.gd
@@ -1,9 +1,11 @@
 extends Control
 
+@export var level_bgm: AudioStream
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if level_bgm:
+		Music.load_and_play_levelbgm(level_bgm)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
