16dcdfe2f07da427e51dd79f0f44c56078d3ef8b
Sergio Ferrer Gimeno <gimenosg@amazon.com>
2023-07-16 13:27:42 +0100

    Coins despawn after 5 secs

--- src/Actors/Pickups/Coin.gd
+++ src/Actors/Pickups/Coin.gd
@@ -3,6 +3,9 @@ class_name Coin
 
 @export var SPEED = 50.0
 @export var score: int = 30
+@export var time_to_despawn: float = 5.0
+
+var elapsed_time: float = 0.0
 
 var coin_pickup = preload("res://src/Audio/Player/slingshot-2.wav")
 
@@ -13,6 +16,11 @@ var dir = Vector2.LEFT
 #	if not Global.is_connected("coin_retrieved", _play_coin_retrieved):
 #		con_res = Global.connect("coin_retrieved", _play_coin_retrieved)
 #		assert(con_res == OK)
+
+func _process(delta: float) -> void:
+	elapsed_time+= delta
+	if (elapsed_time >= time_to_despawn):
+		queue_free()
 
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
